ed784e579f367907892d39e1a954e9e91ecc0caa
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-05-24 13:34:39 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -7,3 +7,17 @@ resource "aws_s3_bucket" "b1" {
     Environment = "Dev"
   }
 }
+resource "aws_s3_bucket_object" "b1" {
+
+  bucket = "awsbucketleena3"
+  
+  key    = "profile2"
+
+  acl    = "public-read"
+
+  source = "th.jpg"
+
+  etag = filemd5("th.jpg")
+
+
+
