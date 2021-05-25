8d7fbd5937377799f519a1794b0f29cc9a382dc0
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-05-25 11:45:29 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,10 +1,23 @@
 resource "aws_s3_bucket" "b1" {
-  bucket = "awsbucketl3"
-  acl    = "public-read"
+  bucket = "awsbucketl4"
+  acl    = "public-read-write"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+resource "aws_s3_bucket_object" "b1" {
+
+  bucket = "awsbucketl4"
+  
+  key    = "profile3"
+
+  acl    = "public-read"
+
+  source = "th.jpg"
+
+  etag = filemd5("th.jpg")
+
 }
 
